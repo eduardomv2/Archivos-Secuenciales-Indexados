c964cc9e13928505a194ea410234921a6746a50f
Eduardo Alejandro Martínez Villalobos <eduardoalejandro.v.mtz@gmail.com>
2024-04-29 07:09:53 -0600

    Agregar archivos de proyecto.

---
+++ Archivos Secuenciales Indexados/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Archivos_Secuenciales_Indexados
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.numIndex = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numIndex)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(152, 50);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(330, 20);
+            this.txtInput.TabIndex = 0;
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Location = new System.Drawing.Point(76, 140);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(75, 23);
+            this.btnWrite.TabIndex = 1;
+            this.btnWrite.Text = "button1";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(37, 237);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(35, 13);
+            this.lblOutput.TabIndex = 2;
+            this.lblOutput.Text = "label1";
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(240, 140);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 23);
+            this.btnRead.TabIndex = 3;
+            this.btnRead.Text = "button1";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(380, 140);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "button1";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(524, 140);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(75, 23);
+            this.btnFind.TabIndex = 5;
+            this.btnFind.Text = "button1";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // numIndex
+            // 
+            this.numIndex.Location = new System.Drawing.Point(507, 51);
+            this.numIndex.Name = "numIndex";
+            this.numIndex.Size = new System.Drawing.Size(120, 20);
+            this.numIndex.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(639, 450);
+            this.Controls.Add(this.numIndex);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.btnWrite);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numIndex)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.NumericUpDown numIndex;
+    }
+}
+
---
+++ Archivos Secuenciales Indexados/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Archivos_Secuenciales_Indexados
+{
+    public partial class Form1 : Form
+    {
+        // Ruta del archivo secuencial
+        private const string filePath = "sequentialFile.txt";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            int index = (int)numIndex.Value;
+            string text = txtInput.Text;
+
+            using (StreamWriter writer = new StreamWriter(filePath, true))
+            {
+                writer.WriteLine($"{index}: {text}");
+            }
+
+            txtInput.Clear();
+            lblOutput.Text = "Text added with index " + index;
+
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(filePath))
+            {
+                string content = File.ReadAllText(filePath);
+                lblOutput.Text = "All file content:\n" + content;
+            }
+            else
+            {
+                lblOutput.Text = "File does not exist.";
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(filePath))
+            {
+                int index = (int)numIndex.Value;
+                var lines = File.ReadAllLines(filePath);
+                var newLines = lines.Where(l => !l.StartsWith($"{index}:")).ToList();
+
+                File.WriteAllLines(filePath, newLines);
+
+                lblOutput.Text = $"Removed index {index}.";
+            }
+            else
+            {
+                lblOutput.Text = "File does not exist.";
+            }
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(filePath))
+            {
+                int index = (int)numIndex.Value;
+                var lines = File.ReadAllLines(filePath);
+                var result = lines.FirstOrDefault(l => l.StartsWith($"{index}:"));
+
+                if (result != null)
+                {
+                    lblOutput.Text = $"Found at index {index}: {result.Split(':')[1].Trim()}";
+                }
+                else
+                {
+                    lblOutput.Text = "Index not found.";
+                }
+            }
+            else
+            {
+                lblOutput.Text = "File does not exist.";
+            }
+        }
+    }
+}
